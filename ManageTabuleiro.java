3f62343b05eb054ac12363120f43dbb1c168f72d
Matilde-Freitas <108094700+Matilde-Freitas@users.noreply.github.com>
2023-03-04 16:43:32 +0000

    Add files via upload

--- ManageTabuleiro.java
+++ ManageTabuleiro.java
@@ -74,6 +74,108 @@ public class ManageTabuleiro
 		return coordenadas_navio;
 	}
 
+	public static int[] gerarCoordenadas2(int tipo_navio, int orientacao, int N, int M)
+	{
+
+		// 0 - tipo S ; 1 - tipo SS ; 2 - tipo SSSS
+		// 0 - norte ; 1 - sul ; 2 - este ; 3 - oeste
+
+		int[] coordenadas_navio = new int[2];
+		int c1 = 0;
+		int c2 = 0;
+		boolean val_coordenada = true;
+
+
+		if (tipo_navio == 0)
+		{
+			c1 = new Random().nextInt(N);
+			c2 = new Random().nextInt(M);
+
+		}
+
+		else if (tipo_navio == 1)
+		{
+			c1 = new Random().nextInt(N);
+			c2 = new Random().nextInt(M);
+
+			if (orientacao == 0)
+			{
+				while (c1 < 1)
+				{
+					c1 = new Random().nextInt(N);
+				}
+			}
+
+			else if (orientacao == 1)
+			{
+				while (c1 >= (N - 1))
+				{
+					c1 = new Random().nextInt(N);
+				}
+
+			}
+
+			else if (orientacao == 2)
+			{
+				while (c2 >= (M - 1))
+				{
+					c2 = new Random().nextInt(M);
+				}
+			}
+
+			else
+			{
+				while (c2 < 1)
+				{
+					c2 = new Random().nextInt(M);
+				}
+			}
+		}
+
+		else
+		{
+			c1 = new Random().nextInt(N);
+			c2 = new Random().nextInt(M);
+
+			if (orientacao == 0)
+			{
+				while (c1 < 3)
+				{
+					c1 = new Random().nextInt(N);
+				}
+			}
+
+			else if (orientacao == 1)
+			{
+				while (c1 >= (N - 3))
+				{
+					c1 = new Random().nextInt(N);
+				}
+			}
+
+			else if (orientacao == 2)
+			{
+				while (c2 >= (M - 3))
+				{
+					c2 = new Random().nextInt(M);
+				}
+			}
+
+			else
+			{
+				while (c2 < 3)
+				{
+					c2 = new Random().nextInt(M);
+				}
+			}
+		}
+
+		coordenadas_navio[0] = c1;
+		coordenadas_navio[1] = c2;
+
+		return coordenadas_navio;
+	}
+
 	public static char[][] criarTabuleiro(int N, int M, char water, int navios_colocados, int navios, char ship)
 	{
 		char[][] tabuleiro = new char[N][M];
@@ -264,54 +366,57 @@ public class ManageTabuleiro
 		return tabuleiro;
 	}
 
-	public static char[][] criarTabuleiro2(int N, int M, char water, int navios_colocados, int navios, char ship)
+	public static char[][] criarTabuleiro2(int N, int M, char water, int navios, char ship)
 	{
 		char[][] tabuleiro = new char[N][M];
 		int tipo_navio;
 		int orientacao;
 		int[] local = new int[2];
 		int tentativa = 0;
-		
+		int navios_colocados = 0;
+
 		char possivel_local;
 		char possivel_local1;
 		char possivel_local2;
 		char possivel_local3;
 		char possivel_local4;
-		
+
 		// 0 - tipo S ; 1 - tipo SS ; 2 - tipo SSSS
-		// 0 - horizontal ; 1 - vertical
-		
+		// 0 - norte ; 1 - sul ; 2 - este ; 3 - oeste
+
 		for (char[] row : tabuleiro)
 		{
 			Arrays.fill(row, water);
 		}
-		
-		while ( navios_colocados < navios && tentativa <= 100 )
+
+		while (navios_colocados < navios && tentativa <= 100)
 		{
 			tentativa++;
-			
-			if (navios_colocados < 1)
+
+			if (navios_colocados == 0)
 			{
 				tipo_navio = 0;
 			}
-			
-			else if (navios_colocados > 0 && navios_colocados < 2)
+
+			else if (navios_colocados == 1)
 			{
 				tipo_navio = 1;
 			}
-			
-			else if (navios_colocados > 1 && navios_colocados < 3)
+
+			else if (navios_colocados == 2)
 			{
 				tipo_navio = 2;
 			}
-			
+
 			else
 			{
 				tipo_navio = new Random().nextInt(3);
 			}
-			
+
 			if (tipo_navio == 0)
 			{
+				orientacao = 0;
+				local = gerarCoordenadas2(tipo_navio, orientacao, N, M);
 				possivel_local = tabuleiro[local[0]][local[1]];
 
 				if (possivel_local == water)
@@ -320,90 +425,155 @@ public class ManageTabuleiro
 					navios_colocados++;
 				}
 			}
-				
+
 			else if (tipo_navio == 1)
 			{
-				orientacao = new Random().nextInt(2);
-				
+				orientacao = new Random().nextInt(4);
+				local = gerarCoordenadas2(tipo_navio, orientacao, N, M);
+
 				if (orientacao == 0)
 				{
 					possivel_local1 = tabuleiro[local[0]][local[1]];
-					possivel_local2 = tabuleiro[local[0]][(local[1] + 1)];
+					possivel_local2 = tabuleiro[local[0] - 1][local[1]];
 
 					if (possivel_local1 == water && possivel_local2 == water)
 					{
 						tabuleiro[local[0]][local[1]] = ship;
-						tabuleiro[local[0]][(local[1] + 1)] = ship;
+						tabuleiro[local[0] - 1][local[1]] = ship;
 						navios_colocados++;
 					}
 				}
-				
-				else
+
+				else if (orientacao == 1)
 				{
 					possivel_local1 = tabuleiro[local[0]][local[1]];
-					possivel_local2 = tabuleiro[local[0]][(local[1] + 1)];
+					possivel_local2 = tabuleiro[local[0] + 1][local[1]];
 
 					if (possivel_local1 == water && possivel_local2 == water)
 					{
 						tabuleiro[local[0]][local[1]] = ship;
-						tabuleiro[local[0]][(local[1] + 1)] = ship;
+						tabuleiro[local[0] + 1][local[1]] = ship;
+						navios_colocados++;
+					}
+				}
+
+				else if (orientacao == 2)
+				{
+					possivel_local1 = tabuleiro[local[0]][local[1]];
+					possivel_local2 = tabuleiro[local[0]][local[1] + 1];
+
+					if (possivel_local1 == water && possivel_local2 == water)
+					{
+						tabuleiro[local[0]][local[1]] = ship;
+						tabuleiro[local[0]][local[1] + 1] = ship;
+						navios_colocados++;
+					}
+				}
+
+				else
+				{
+					possivel_local1 = tabuleiro[local[0]][local[1]];
+					possivel_local2 = tabuleiro[local[0]][local[1] - 1];
+
+					if (possivel_local1 == water && possivel_local2 == water)
+					{
+						tabuleiro[local[0]][local[1]] = ship;
+						tabuleiro[local[0]][local[1] - 1] = ship;
 						navios_colocados++;
 					}
 				}
 			}
-				
+
 			else if (tipo_navio == 2)
 			{
-				orientacao = new Random().nextInt(2);
-				
+				orientacao = new Random().nextInt(4);
+				local = gerarCoordenadas2(tipo_navio, orientacao, N, M);
+
 				if (orientacao == 0)
 				{
 					possivel_local1 = tabuleiro[local[0]][local[1]];
-					possivel_local2 = tabuleiro[local[0]][(local[1] + 1)];
-					possivel_local3 = tabuleiro[local[0]][(local[1] + 2)];
-					possivel_local4 = tabuleiro[local[0]][(local[1] + 3)];
+					possivel_local2 = tabuleiro[local[0] - 1][local[1]];
+					possivel_local3 = tabuleiro[local[0] - 2][local[1]];
+					possivel_local4 = tabuleiro[local[0] - 3][local[1]];
 
 					if (possivel_local1 == water && possivel_local2 == water && possivel_local3 == water
 							&& possivel_local4 == water)
 					{
 						tabuleiro[local[0]][local[1]] = ship;
-						tabuleiro[local[0]][(local[1] + 1)] = ship;
-						tabuleiro[local[0]][(local[1] + 2)] = ship;
-						tabuleiro[local[0]][(local[1] + 3)] = ship;
+						tabuleiro[local[0] - 1][local[1]] = ship;
+						tabuleiro[local[0] - 2][local[1]] = ship;
+						tabuleiro[local[0] - 3][local[1]] = ship;
 						navios_colocados++;
 					}
 				}
-				
-				else
+
+				else if (orientacao == 1)
 				{
 					possivel_local1 = tabuleiro[local[0]][local[1]];
-					possivel_local2 = tabuleiro[(local[0] + 1)][local[1]];
-					possivel_local3 = tabuleiro[(local[0] + 2)][local[1]];
-					possivel_local4 = tabuleiro[(local[0] + 3)][local[1]];
+					possivel_local2 = tabuleiro[local[0] + 1][local[1]];
+					possivel_local3 = tabuleiro[local[0] + 2][local[1]];
+					possivel_local4 = tabuleiro[local[0] + 3][local[1]];
 
 					if (possivel_local1 == water && possivel_local2 == water && possivel_local3 == water
 							&& possivel_local4 == water)
 					{
 						tabuleiro[local[0]][local[1]] = ship;
-						tabuleiro[(local[0] + 1)][local[1]] = ship;
-						tabuleiro[(local[0] + 2)][local[1]] = ship;
-						tabuleiro[(local[0] + 3)][local[1]] = ship;
+						tabuleiro[local[0] + 1][local[1]] = ship;
+						tabuleiro[local[0] + 2][local[1]] = ship;
+						tabuleiro[local[0] + 3][local[1]] = ship;
+						navios_colocados++;
+					}
+				}
+
+				else if (orientacao == 2)
+				{
+					possivel_local1 = tabuleiro[local[0]][local[1]];
+					possivel_local2 = tabuleiro[local[0]][local[1] + 1];
+					possivel_local3 = tabuleiro[local[0]][local[1] + 2];
+					possivel_local4 = tabuleiro[local[0]][local[1] + 3];
+
+					if (possivel_local1 == water && possivel_local2 == water && possivel_local3 == water
+							&& possivel_local4 == water)
+					{
+						tabuleiro[local[0]][local[1]] = ship;
+						tabuleiro[local[0]][local[1] + 1] = ship;
+						tabuleiro[local[0]][local[1] + 2] = ship;
+						tabuleiro[local[0]][local[1] + 3] = ship;
+						navios_colocados++;
+					}
+				}
+
+				else
+				{
+					possivel_local1 = tabuleiro[local[0]][local[1]];
+					possivel_local2 = tabuleiro[local[0]][local[1] - 1];
+					possivel_local3 = tabuleiro[local[0]][local[1] - 2];
+					possivel_local4 = tabuleiro[local[0]][local[1] - 3];
+
+					if (possivel_local1 == water && possivel_local2 == water && possivel_local3 == water
+							&& possivel_local4 == water)
+					{
+						tabuleiro[local[0]][local[1]] = ship;
+						tabuleiro[local[0]][local[1] - 1] = ship;
+						tabuleiro[local[0]][local[1] - 2] = ship;
+						tabuleiro[local[0]][local[1] - 3] = ship;
 						navios_colocados++;
 					}
 				}
 			}
 		}
-		
-		if (tentativa == 100)
+
+		if (tentativa > 100)
 		{
-			System.out.print("Excedeu o número máximo de tentativas de colocação. Por favor inserir novos valores iniciais");
+			System.out.println(
+					"Excedeu o número máximo de tentativas de colocação. Por favor inserir novos valores iniciais");
 			tabuleiro = new char[2][2];
 		}
-		
+
 		return tabuleiro;
-			
+
 	}
-	
+
 	public static void printTabuleiro(char[][] tabuleiro, char water, char ship, int N, int M)
 	{
 		System.out.print("    ");
@@ -438,23 +608,22 @@ public class ManageTabuleiro
 			for (int col = 0; col < M; col++)
 			{
 				char posicao = tabuleiro[row][col];
-				
-				if (posicao == ship)
-				{
-					System.out.print(water + "  ");
-				}
-				
-				else
-				{
-					System.out.print(posicao + "  ");
-				}
+
+				// if (posicao == ship)
+				// {
+				// System.out.print(water + " ");
+				// }
+				//
+				// else
+				// {
+				System.out.print(posicao + "  ");
+				// }
 			}
 
 			System.out.println();
 		}
 	}
 
-	
 	public static int[] inserirCoordenadas(int N, int M, Scanner scan)
 	{
 		int row = 0;
@@ -532,7 +701,6 @@ public class ManageTabuleiro
 
 	}
 
-	
 	public static char avaliarShot(int[] coordenadasJogador, char[][] tabuleiro, char ship, char water, char hit,
 			char miss)
 	{
@@ -565,7 +733,6 @@ public class ManageTabuleiro
 
 	}
 
-	
 	public static char[][] atualizarTabuleiro(char[][] tabuleiro, int[] coordenadasJogador, char resultadoShot)
 	{
 		int row = coordenadasJogador[0];
@@ -573,8 +740,7 @@ public class ManageTabuleiro
 		tabuleiro[row][col] = resultadoShot;
 		return tabuleiro;
 	}
-	
-	
+
 	public static int contarS(char[][] tabuleiro, char ship, int N, int M)
 	{
 		int numS = 0;
@@ -593,7 +759,6 @@ public class ManageTabuleiro
 		return numS;
 	}
 
-	
 	public static boolean iniciarNovoJogo(boolean val_interacao, Scanner scan)
 	{
 		boolean val_escolha = true;
@@ -629,25 +794,25 @@ public class ManageTabuleiro
 	{
 		boolean val_escolha = true;
 		String i;
-		
+
 		System.out.println("Prima enter para continuar a jogar ou escreva 'quit' para abandonar a partida.");
 
 		while (val_escolha)
 		{
 			i = scan.nextLine();
-			
+
 			if (i.equals(""))
 			{
 				val_escolha = false;
 				val_interacao = true;
 			}
-			
+
 			else if (i.equals("quit"))
 			{
 				val_escolha = false;
 				val_interacao = false;
 			}
-		
+
 			else
 			{
 				System.out.println("Valor inválido");
@@ -656,8 +821,5 @@ public class ManageTabuleiro
 		}
 		return val_interacao;
 	}
-	
-//	public static boolean tabuleiroCheck(boolean)
-
 
 }
--- main.java
+++ main.java
@@ -8,7 +8,7 @@ public class main
 	public static void main(String[] args)
 	{
 		Scanner scan = new Scanner(System.in);
-		
+
 		boolean val_menu = true;
 		boolean val_N = true;
 		boolean val_M = true;
@@ -17,18 +17,17 @@ public class main
 		boolean val_interacao = true;
 		boolean val_tabuleiro_valido = true;
 		boolean val_jogo = true;
-		
+
 		char water = '.';
 		char ship = 'S';
 		char hit = 'X';
 		char miss = 'o';
-		
+
 		char[] resultado = new char[100];
 		char vitoria = 'V';
 		char derrota = 'D';
 		char abandono = 'A';
-		
-		int navios_colocados = 0;
+
 		int numJogo = 0;
 		int numS = 0;
 		int[] jogadasTotal = new int[100];
@@ -49,98 +48,99 @@ public class main
 			else if (menu == 2)
 			{
 				System.out.println();
-				ManageStats.printStatsTotal(resultado,numJogo, jogadasTotal);
+				ManageStats.printStatsTotal(resultado, numJogo, jogadasTotal);
 				System.out.println();
 				ManageStats.printPerc(resultado, numJogo, jogadasTotal, vitoria, derrota, abandono);
 				System.out.println();
 				ManageStats.printMed(jogadasTotal, numJogo);
-				
+
 				val_menu = true;
 			}
 
 			else if (menu == 1)
 			{
 				System.out.println("GAME START");
-				
-				while(val_jogo)
+
+				while (val_jogo)
 				{
 					val_tabuleiro_valido = true;
 					val_interacao = true;
-					
+
 					System.out.println("Defina as dimensões do tabuleiro.");
-	
+
 					int N = inputs.check_N(val_N, scan);
-	
+
 					int M = inputs.check_M(val_M, scan);
-	
+
 					int navios = inputs.check_navios(val_navios, scan);
-					
+
 					char tabuleiro[][] = new char[N][M];
-					
+
 					while (val_interacao)
 					{
+						tabuleiro = ManageTabuleiro.criarTabuleiro2(N, M, water, navios, ship);
 
-						tabuleiro = ManageTabuleiro.criarTabuleiro(N, M, water, navios_colocados, navios, ship);
-						
-						if (tabuleiro.length == 2)
+						if (tabuleiro.length < 10)
 						{
 							val_tabuleiro_valido = false;
 							val_interacao = false;
 						}
-						
-						if (val_tabuleiro_valido = true)
+
+						if (val_tabuleiro_valido == true)
 						{
 							numS = ManageTabuleiro.contarS(tabuleiro, ship, N, M);
-							
+
 							int max_shot = inputs.check_shot(val_shot, scan, numS, N, M);
-							
+
 							ManageTabuleiro.printTabuleiro(tabuleiro, water, ship, N, M);
-		
+
 							int jogadas = 0;
-		
+
 							numJogo++;
-		
+
 							for (int i = 0; i < max_shot; i++)
 							{
 								int[] coordenadasJogador = ManageTabuleiro.inserirCoordenadas(N, M, scan);
 								jogadas++;
-								char resultadoShot = ManageTabuleiro.avaliarShot(coordenadasJogador, tabuleiro, ship, water, hit, miss);
-		
-								tabuleiro = ManageTabuleiro.atualizarTabuleiro(tabuleiro, coordenadasJogador, resultadoShot);
+								char resultadoShot = ManageTabuleiro.avaliarShot(coordenadasJogador, tabuleiro, ship,
+										water, hit, miss);
+
+								tabuleiro = ManageTabuleiro.atualizarTabuleiro(tabuleiro, coordenadasJogador,
+										resultadoShot);
 								ManageTabuleiro.printTabuleiro(tabuleiro, water, ship, N, M);
-		
+
 								numS = ManageTabuleiro.contarS(tabuleiro, ship, N, M);
-		
+
 								if (numS == 0)
 								{
 									System.out.println("Ganhou o jogo!");
 									resultado[numJogo] = vitoria;
 									jogadasTotal[numJogo] = jogadas;
-		
+
 									val_interacao = ManageTabuleiro.iniciarNovoJogo(val_interacao, scan);
 									if (val_interacao == false)
 									{
 										val_jogo = false;
 									}
 									i = max_shot;
-		
+
 								}
-		
+
 								else if (jogadas == max_shot)
 								{
 									System.out.println("Perdeu o jogo");
 									resultado[numJogo] = derrota;
 									jogadasTotal[numJogo] = jogadas;
-		
+
 									val_interacao = ManageTabuleiro.iniciarNovoJogo(val_interacao, scan);
 									if (val_interacao == false)
 									{
 										val_jogo = false;
 									}
 									i = max_shot;
-		
+
 								}
-		
+
 								if (i < max_shot)
 								{
 									val_interacao = ManageTabuleiro.abandonarJogo(val_interacao, scan);
@@ -160,7 +160,7 @@ public class main
 						}
 					}
 				}
-				
+
 				val_menu = true;
 			}
 
@@ -169,7 +169,7 @@ public class main
 				System.out.println("Erro: Não insiriu um número correto");
 				val_menu = true;
 			}
-			
+
 			System.out.println();
 		}
 
